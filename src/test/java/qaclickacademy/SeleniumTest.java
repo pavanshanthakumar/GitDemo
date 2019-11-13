2a7edbe56041414be5b40317eaab9fec731b2edb
pavan <s.pavan035@gmail.com>
2019-11-13 15:47:06 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	//TestNG XML file from Maven
+		//How to execute all test cases from Test folder - mvn test
+		@Test
+		public void BrowserAUtomation()
+		{
+			System.out.println("BrowserAUtomation");	
+		}
+		@Test
+		public void elementsUi()
+		{
+			System.out.println("elementsUi");	
+		}
+}
